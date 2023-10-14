17c365f54508fc8b5e156955d85eae942fd1aed3
Jenil Savani <jenilpatel9913@gmail.com>
2023-10-14 15:42:14 +0530

    Implemented other sections

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { BallCanvas } from './canvas';
+import { SectionWrapper } from '../HOC';
+import { technologies } from '../constants';
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className='flex flex-row flex-wrap justify-center gap-10'>
+      {technologies.map((technology) => (
+        <div className='w-28 h-28' key={technology.icon}>
+          <BallCanvas icon={technology.icon} />
+        </div>
+      ))}
+    </div>
   )
 }
 
